e31c5c57faed4f2003100ea6f51cd74bd418d4cc
lichmi <1090382462@qq.com>
2020-11-15 21:56:10 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+// 加载vant核心组件库
+import Vant from 'vant'
+// 加载vant 全局样式
+import 'vant/lib/index.css'
+// 注册使用vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
